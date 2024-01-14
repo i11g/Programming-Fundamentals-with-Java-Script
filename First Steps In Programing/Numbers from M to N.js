c96c41a347060eea72bb10bd8d9d9a56cf26d092
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-01-14 16:33:08 +0200

    Add files via upload

---
+++ First Steps In Programing/Numbers from M to N.js
@@ -0,0 +1,10 @@
+function numbers(numM, numN) { 
+
+    for (let index = numM; index>=numN; index--) 
+    {
+        console.log(index);
+        
+    }
+}
+
+numbers(4, 1 )
--- First Steps In Programing/Theatre Promotions.js
+++ First Steps In Programing/Theatre Promotions.js
@@ -1,3 +1,63 @@
-function theatre () {
+function theatre (day,age) {
+    
+    let ticketprice;
+
+    if (age>=0&&age<=18) 
+    { 
+        if(day==="Weekday") 
+        {
+            ticketprice="12$";
+        }
+        else if(day==="Weekend")
+        {
+            ticketprice="15$";
+        }
+        else if(day==="Holiday")
+        {
+            ticketprice="$5";
+        }
+    }
+    else if(age>18&&age<=64) 
+    { 
+        if(day==="Weekday") 
+        {
+            ticketprice="$18";
+        }
+        else if(day==="Weekend")
+        {
+            ticketprice="$20";
+        }
+        else if(day==="Holiday")
+        {
+            ticketprice="$12";
+        }
+    }
+    else if(age>64&&age<=122) 
+    { 
+        if(day==="Weekday") 
+        {
+            ticketprice="$12";
+        }
+        else if(day==="Weekend")
+        {
+            ticketprice="$15";
+        }
+        else if(day==="Holiday")
+        {
+            ticketprice="$10";
+        }
+    }     
+    else 
+    { 
+
+        console.log("Error!");
+    }
+    if(ticketprice!==undefined) 
+    {
+        console.log(ticketprice);
+    }
+       
     
 }
+
+theatre('Holiday', -12)
