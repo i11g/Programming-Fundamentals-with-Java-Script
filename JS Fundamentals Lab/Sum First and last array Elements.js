4a807e515dfe2b8af0e44cbd4f453a259e023b77
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-01-20 12:08:36 +0200

    Add files via upload

---
+++ JS Fundamentals Lab/Sum First and last array Elements.js
@@ -0,0 +1,10 @@
+function sum(input) { 
+
+    let firstElement=input[0];
+    let lastElement=input[input.length-1];
+
+    let sum=firstElement+lastElement;
+    console.log(sum);
+
+}
+sum([20, 30, 40] )
